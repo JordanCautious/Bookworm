4a04a70a9d15d573e758f87d2a31fe8ccd560ba8
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2022-07-13 14:17:12 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Jordan Haynes on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
